5aa657a3cc3d500287c6de04fc29654bfbf1e7b0
Grims <24793858+chillithechillinchinchilla@users.noreply.github.com>
2023-10-13 14:11:42 -0400

    Update Navbar.jsx
    add basic inventory link
    fix homepage link

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,212 +1,222 @@
 import React from "react";
 {
-  /* eslint-disable*/
+    /* eslint-disable*/
 }
 function DarkNavbar() {
-  return (
-    <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          Product Reviews
-        </a>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            {/* ----------------------- */}
-            {/* Communications Section  */}
-            {/* ----------------------- */}
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Communications
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
-            {/* ----------------------- */}
-            {/* Inventory Section  */}
-            {/* ----------------------- */}
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Inventory
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Inventory Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another Inventory action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
+    return (
+        <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">
+                    Product Reviews
+                </a>
+                <div
+                    className="collapse navbar-collapse"
+                    id="navbarNavDropdown"
+                >
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a
+                                className="nav-link active"
+                                aria-current="page"
+                                href="/"
+                            >
+                                Home
+                            </a>
+                        </li>
+                        {/* ----------------------- */}
+                        {/* Communications Section  */}
+                        {/* ----------------------- */}
+                        <li className="nav-item dropdown">
+                            <a
+                                className="nav-link dropdown-toggle"
+                                href="#"
+                                role="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                Communications
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li>
+                                    <a className="dropdown-item" href="#">
+                                        Action
+                                    </a>
+                                </li>
+                                <li>
+                                    <a className="dropdown-item" href="#">
+                                        Another action
+                                    </a>
+                                </li>
+                                <li>
+                                    <a className="dropdown-item" href="#">
+                                        Something else here
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        {/* ----------------------- */}
+                        {/* Inventory Section  */}
+                        {/* ----------------------- */}
+                        <li className="nav-item dropdown">
+                            <a
+                                className="nav-link dropdown-toggle"
+                                href="#"
+                                role="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                Inventory
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li>
+                                    <a
+                                        className="dropdown-item"
+                                        href="/products"
+                                    >
+                                        Basic Inventory
+                                    </a>
+                                </li>
+                                <li>
+                                    <a className="dropdown-item" href="#">
+                                        Another Inventory action
+                                    </a>
+                                </li>
+                                <li>
+                                    <a className="dropdown-item" href="#">
+                                        Something else here
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
 
-            {/* ----------------------- */}
-            {/* Lab Section  */}
-            {/* ----------------------- */}
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Lab
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Lab Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another Lab action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
+                        {/* ----------------------- */}
+                        {/* Lab Section  */}
+                        {/* ----------------------- */}
+                        <li className="nav-item dropdown">
+                            <a
+                                className="nav-link dropdown-toggle"
+                                href="#"
+                                role="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                Lab
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li>
+                                    <a className="dropdown-item" href="#">
+                                        Lab Action
+                                    </a>
+                                </li>
+                                <li>
+                                    <a className="dropdown-item" href="#">
+                                        Another Lab action
+                                    </a>
+                                </li>
+                                <li>
+                                    <a className="dropdown-item" href="#">
+                                        Something else here
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
 
-            {/* ----------------------- */}
-            {/* Reviews / Awards Section  */}
-            {/* ----------------------- */}
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Reviews/Awards
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Reviews Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another Award action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
+                        {/* ----------------------- */}
+                        {/* Reviews / Awards Section  */}
+                        {/* ----------------------- */}
+                        <li className="nav-item dropdown">
+                            <a
+                                className="nav-link dropdown-toggle"
+                                href="#"
+                                role="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                Reviews/Awards
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li>
+                                    <a className="dropdown-item" href="#">
+                                        Reviews Action
+                                    </a>
+                                </li>
+                                <li>
+                                    <a className="dropdown-item" href="#">
+                                        Another Award action
+                                    </a>
+                                </li>
+                                <li>
+                                    <a className="dropdown-item" href="#">
+                                        Something else here
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
 
-            {/* ----------------------- */}
-            {/* Admin Section  */}
-            {/* ----------------------- */}
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Admin
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Admin Action
-                  </a>
-                </li>
+                        {/* ----------------------- */}
+                        {/* Admin Section  */}
+                        {/* ----------------------- */}
+                        <li className="nav-item dropdown">
+                            <a
+                                className="nav-link dropdown-toggle"
+                                href="#"
+                                role="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                Admin
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li>
+                                    <a className="dropdown-item" href="#">
+                                        Admin Action
+                                    </a>
+                                </li>
 
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
+                                <li>
+                                    <a className="dropdown-item" href="#">
+                                        Something else here
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
 
-            {/* ----------------------- */}
-            {/* Prototype Section  */}
-            {/* ----------------------- */}
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Prototype
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Prototype Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another Inventory action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+                        {/* ----------------------- */}
+                        {/* Prototype Section  */}
+                        {/* ----------------------- */}
+                        <li className="nav-item dropdown">
+                            <a
+                                className="nav-link dropdown-toggle"
+                                href="#"
+                                role="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                Prototype
+                            </a>
+                            <ul className="dropdown-menu">
+                                <li>
+                                    <a className="dropdown-item" href="#">
+                                        Prototype Action
+                                    </a>
+                                </li>
+                                <li>
+                                    <a className="dropdown-item" href="#">
+                                        Another Inventory action
+                                    </a>
+                                </li>
+                                <li>
+                                    <a className="dropdown-item" href="#">
+                                        Something else here
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
 export default DarkNavbar;
